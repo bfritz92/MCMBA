5e63db41761cd57595b773531ba8df2e06eca8f2
brian <fritz@jfmd.org>
2022-07-27 10:11:04 -0400

    more stackoverflow fun

--- functions.php
+++ functions.php
@@ -86,19 +86,63 @@ add_action( 'widgets_init', 'arphabet_widgets_init' );
 
 
 //add child pages shortcode
-function wpb_list_child_pages() { 
+function show_childpages_shortcode() {
 
-    global $post; 
+    // a shortcode should just return the content not echo html
+    // so we start to create an object, and on the end we return it
+    // if we dont do this the shortcode will be displayed in the top of the content
+    ob_start();
 
-    $id = ( is_page() && $post->post_parent ) ? $post->post_parent : $post->ID;
-    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $id . '&echo=0' );
-    //you can add `&depth=1` in the end, so it only shows one level
+    // only start if we are on a single page
+    if ( is_page() ) {
 
-    if ( $childpages ) {    
-        $string = '<ul>' . $childpages . '</ul>';
-    }
+        // get the ID of the current (parent) page
+        $current_page_id = get_the_ID();
 
-    return $string;
+        // get all the children of the current page
+        $child_pages = get_pages( array( 
+            'child_of' => $current_page_id,  
+        ) );
+
+        // start only if we have some childpages
+        if ($child_pages) {
+
+            // if we have some children, display a list wrapper
+            echo '<ul class="childpages">';
+
+            // loop trough each childpage 
+            foreach ($child_pages as $child_page) {
+
+                $page_id    = $child_page->ID; // get the ID of the childpage
+                $page_link  = get_permalink( $page_id ); // returns the link to childpage
+                $page_img   = get_the_post_thumbnail( $page_id, 'medium' ); // returns the featured image <img> element
+                $page_title = $child_page->post_title; // returns the title of the child page
+
+                ?>
+
+                <li class="childpage-<?php echo $page_id; ?>">
+
+                    <a href="<?php echo $page_link; ?>">
+
+                        <h4><?php echo $page_title; ?></h4>
+                        <?php echo $page_img; //display featured image ?>
+
+                    </a>
+
+                </li>
+
+                <?php
+
+            }//END foreach ($child_pages as $child_page)
+
+            echo '</ul>';
+
+        }//END if ($child_pages)    
+
+    }//END if (is_page())
+
+    // return the object
+    return ob_get_clean();
+
 }
-
-add_shortcode('wpb_childpages', 'wpb_list_child_pages');
+add_shortcode( 'show_childpages', 'show_childpages_shortcode' );
